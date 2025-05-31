64b6b37adb5bef3773fddc7ad837783d68c3e053
abidjim009 <abidhasanjim1@gmail.com>
2025-05-31 16:44:57 +0600

    Initial commit

---
+++ day78.py
@@ -0,0 +1,46 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+    def make_sound(self):
+        print("Sound made by the animal")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
+        
+    def make_sound(self):
+        print("Bark!")
+
+d = Dog("Dog", "Doggerman")
+d.make_sound()
+
+a = Animal("Dog", "Dog")
+a.make_sound()
+
+# Quick Quiz: Implement a Cat class by using the animal class. Add some methods specific to cat
+'''
+Quick quiz:
+
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def make_sound(self):
+        print("Sound made by the animal")
+
+
+class Cat(Animal):
+    def __init__(self, name, catage, species):
+        super().__init__(name, species)
+        self.catage = catage
+        
+    def make_sound(self):
+        print("meow")
+        
+    def hi(self):
+        print("hi")
+        '''
